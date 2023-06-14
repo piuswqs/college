f09f9739736c35cccfab18447732c9557993eab9
Valeri <valeriya.didyk045@gmail.com>
2023-06-14 08:44:50 +0600

    some commit

---
+++ app/src/main/java/com/example/smartlab/RegistrationWithDopInfoActivity.java
@@ -0,0 +1,28 @@
+package com.example.smartlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegistrationWithDopInfoActivity extends AppCompatActivity {
+TextView btnSkip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_with_dopinfo);
+
+        TextView btnSkip = findViewById(R.id.btnSkipReg);
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), bottom_navigation.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
